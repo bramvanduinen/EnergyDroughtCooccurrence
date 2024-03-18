b6a756113f74ed75163b62cad699306f3c6eab43
Bram van Duinen <bramvduinen@gmail.com>
2024-03-18 15:17:28 +0000

    added find_energydroughts scripts from Lieke

---
+++ src/find_energydroughts/config.py
@@ -0,0 +1,33 @@
+# config.py
+
+# Constants
+FOLDER = "/net/pc200256//nobackup/users/most/output/LENTIS_2023_PD/agg_production/per_country/"
+RUNNAME = "LENTIS_2023_PD"
+OFOLDER = "/usr/people/duinen/MSc-thesis/src/energydroughts-Europe/data/"
+EVENTTYPE = "random"
+COUNTRIES = [
+    "AUT",
+    "BEL",
+    "CHE",
+    "CZE",
+    "DEU",
+    "DNK",
+    "ESP",
+    "EST",
+    "FIN",
+    "FRA",
+    "GBR",
+    "HRV",
+    "HUN",
+    "IRL",
+    "ITA",
+    "LTU",
+    "LVA",
+    "NLD",
+    "NOR",
+    "POL",
+    "PRT",
+    "SVK",
+    "SVN",
+    "SWE",
+]
